ea38d513be3fbb5dc6b45ad3ababa2346c421334
lokeshrookie <rocklinglokesh@gmail.com>
2023-12-04 03:28:41 +0530

    Add project files.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+using WebApi.Services;
+
+
+
+namespace WebApi.Controllers
+{
+    public class ContactController : ApiController
+    {
+
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+
+    }
+}
